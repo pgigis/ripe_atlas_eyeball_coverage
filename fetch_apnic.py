55a141f8c86e1d9ff11ea7196b8e5c27a15f27ea
Petros Gigis <gkigkis@ics.forth.gr>
2019-08-02 17:41:26 +0300

    resolve ASN number to ASN name using ripestat

--- fetch_apnic.py
+++ fetch_apnic.py
@@ -11,31 +11,31 @@ from threading import Thread, Lock
 
 # Initializing paths
 sys.path.append("%s/lib" % ( os.path.dirname(os.path.realpath(__file__) ) ) )
-cur_dict = os.path.dirname(os.path.realpath(__file__))
+root_path = os.path.dirname(os.path.realpath(__file__))
 
 parallel_threads = 8
 
 
 # parse command line arguments
 parser = argparse.ArgumentParser(description="Fetch the APNIC estimates per country")
-parser.add_argument('-c', '--country', dest='country', type=str, help="Country code to study (default = all)", default="all")
-parser.add_argument('-p', '--cumulative_percent', dest='cumulative_percent', type=float, help="Threshold of cumulative percentage for population being covered (default = 95)", default=95)
-parser.add_argument('-t', '--threshold_of_marketshare', dest='threshold_of_marketshare', type=float, help="Threshold to consider a network as eyeball (default = 0.1)", default=0.1)
+parser.add_argument("-c", "--country", dest="country", type=str, help="Country code to study (default = all)", default="all")
+parser.add_argument("-p", "--cumulative_percent", dest="cumulative_percent", type=float, help="Threshold of cumulative percentage for population being covered (default = 95)", default=95)
+parser.add_argument("-t", "--threshold_of_marketshare", dest="threshold_of_marketshare", type=float, help="Threshold to consider a network as eyeball (default = 0.1)", default=0.1)
 
 args = parser.parse_args()
 
 
-def save_networks(country, list_of_eyeball_networks):
-	file_name = cur_dict + '/apnic_' + str(args.cumulative_percent) + "_" + str(args.threshold_of_marketshare) + "/" + country + ".json"
-	with open( file_name , 'w') as outfile:
-		json.dump(list_of_eyeball_networks, outfile)
+def save_networks(country, list_of_eyeballs):
+    file_name = root_path + "/apnic_" + str(args.cumulative_percent) + "_" + str(args.threshold_of_marketshare) + "/" + country + ".json"
+    with open( file_name , "w") as outfile:
+        json.dump(list_of_eyeballs, outfile)
 
 
 def fetch_data(country, cumulative_percent, threshold_of_marketshare):
-    
     APNIC_ECON_URL = "http://v6data.data.labs.apnic.net/ipv6-measurement/Economies/%s/%s.asns.json?m=%s" % (country, country, threshold_of_marketshare)
+    RIPESTAT_NAME_URL = "https://stat.ripe.net/data/whois/data.json?resource="
     econ_data = []
-    list_of_eyeball_networks = list()
+    list_of_eyeballs = list()
 
     try:
         req = urllib2.Request( APNIC_ECON_URL )
@@ -44,58 +44,68 @@ def fetch_data(country, cumulative_percent, threshold_of_marketshare):
     except:
         print "data URL unavailable, exiting (URL:%s)" % APNIC_ECON_URL
 
-    if len( econ_data ) == 0:
+    if len(econ_data) == 0:
         print "data URL returned 0 ASNs for this economy (URL:%s)" % APNIC_ECON_URL
         return False
-    
+
     else:
-        for data in econ_data:
+        for entry in econ_data:
+            ripestat_response = None
+            try:
+                req = urllib2.Request(RIPESTAT_NAME_URL + str(entry["as"]))
+                conn = urllib2.urlopen(req)
+                ripestat_response = json.load(conn)
+                if "data" in ripestat_response:
+                    if "records" in ripestat_response["data"] and len(ripestat_response["data"]["records"]) > 0:
+                        for record in ripestat_response["data"]["records"][0]:
+                            if "key" in record and record["key"] == "as-name":
+                                entry["autnum"] = record["value"]
+                                break
+            except:
+                print "Failed to call RIPEStat" % RIPESTAT_NAME_URL + str(entry["as"])
+                entry["autnum"] = "Not resolved" 
 
-			# delete unnecessary fields
-			del (data['cc'])
+            del entry['cc']
 
-			#print "Selected '" + str(data['autnum']) +"' with population percentage " + str(data['percent'])
-			list_of_eyeball_networks.append(data)
+            list_of_eyeballs.append(entry)
 
-			if data['cumulative'] >= cumulative_percent:
-				break
-
-	save_networks(country, list_of_eyeball_networks)
+            if entry["cumulative"] >= cumulative_percent:
+                break
+    save_networks(country, list_of_eyeballs)
 
 
-def fetch_country_codes(cur_dict):
-	file_path = cur_dict + '/data/countries.txt'
-	with open( file_path ) as f:
-		lines = f.readlines()
-	return  [(line.strip('\n')) for line in lines]
+def fetch_country_codes(root_path):
+    file_path = root_path + "/data/countries.txt"
+    with open( file_path ) as f:
+        lines = f.readlines()
+    return  [(line.strip("\n")) for line in lines]
 
 
 def main():
-	
-	file_path = cur_dict + '/apnic_' + str(args.cumulative_percent) + "_" + str(args.threshold_of_marketshare)
-	if not os.path.exists(file_path):
-		os.makedirs(file_path)
+    
+    file_path = root_path + "/apnic_" + str(args.cumulative_percent) + "_" + str(args.threshold_of_marketshare)
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
 
-	if(args.country == "all"):
+    if(args.country == "all"):
 
-		list_of_threads = list()
-		list_of_countries = fetch_country_codes(cur_dict)
-	
-		for country in list_of_countries:
-			list_of_threads.append(Thread(target=fetch_data, args=( ( country, args.threshold_of_marketshare, args.cumulative_percent ))))
-		
-		for (i,thread) in enumerate(list_of_threads):
-			thread.start()
+        list_of_threads = list()
+        list_of_countries = fetch_country_codes(root_path)
 
-			if(i % parallel_threads == 0):
-				time.sleep(5)
-		
-		for thread in list_of_threads:
-			thread.join()
-	
-	else:
+        for country in list_of_countries:
+            list_of_threads.append(Thread(target=fetch_data, args=( ( country, args.cumulative_percent, args.threshold_of_marketshare ))))
 
-		fetch_data(args.country)
+        for (i,thread) in enumerate(list_of_threads):
+            thread.start()
+
+            if(i % parallel_threads == 0):
+                time.sleep(5)
+
+        for thread in list_of_threads:
+            thread.join()
+
+    else:
+        fetch_data(args.country, args.cumulative_percent, args.threshold_of_marketshare)
 
 
 main()
